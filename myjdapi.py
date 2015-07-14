9c9af769bf3185d68d8cb8ef16fcb230519319bb
mmarquezs <mmsa1994@gmail.com>
2015-07-14 21:37:27 +0200

    Converted to a class, added function __secretcreate and finished (not tested)

--- myjdapi.py
+++ myjdapi.py
@@ -1,19 +1,30 @@
+import hashlib
+
 class jdapi:
     api_url = "http://api.jdownloader.org"
     rid_counter = 0
-    appkey = "MYJDAPI_php";
+    appkey = "myjdapi-python"
     apiVer = 1
     devices = []
-    loginSecret = "";
-    deviceSecret = "";
-    sessiontoken = "";
-    regaintoken = "";
+    loginSecret = ""
+    deviceSecret = ""
+    sessiontoken = ""
+    regaintoken = ""
     serverEncryptionToken = ""
     deviceEncryptionToken = ""
-    SERVER_DOMAIN = "server"
-    DEVICE_DOMAIN = "device";
+
     def __init__(self,email=None,password=None):
-        self.data = []
+        self.rid_counter=int(time.time())
+        if email!=None & password!=None:
+            self.connect(email,password)
+    def __secretcreate(email,password,domain):
+        # Calculate the loginSecret and deviceSecret
+        # email,password, domain (server,device)
+        h = hashlib.sha256()
+        h.update(email.lower()+password+domain.lower())
+        secret=h.hexdigest()
+        return secret
+        
     def connect(email,password):
         # Establish connection to api
         pass
