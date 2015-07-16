b3e12c7298d4e450622ede3e7f06552a2c05e6bd
mmarquezs <mmsa1994@gmail.com>
2015-07-16 13:23:45 +0200

    function call() to make the calls to the api.

--- myjdapi.py
+++ myjdapi.py
@@ -10,8 +10,30 @@ from Crypto.Cipher import AES
 
 unpad = lambda s : s[0:-s[-1]]
 
-class myjdapi:
+class jddevice:
+    """
+    Class that represents a JD device and it's functions
+    
+    """
+    def __init__(self,deviceDict):
+        """ This functions initializates the device instance.
+        It uses the provided dictionary to create the device.
 
+        :param deviceDict: Device dictionary
+        
+        """
+        pass
+    def addLinks(self):
+        pass
+    
+
+
+class myjdapi:
+    """
+    Main class for connecting to JD API.
+
+    """
+    
     def __init__(self,email=None,password=None):
         """ This functions initializates the myjdapi object.
         If email and password are given it will also connect try 
@@ -33,19 +55,20 @@ class myjdapi:
         self.deviceSecret = ""
         self.sessiontoken = ""
         self.regaintoken = ""
-        self.serverEncryptionToken = ""
-        self.deviceEncryptionToken = ""
+        self.serverEncryptionToken = False
+        self.deviceEncryptionToken = False
 
         if email!=None and password!=None:
             self.connect(email,password)
             # Make an exception or something if it fails? Or simply ignore the error?
     def __secretcreate(self,email,password,domain):
-        """ 
-        Calculates the loginSecret and deviceSecret
+        """Calculates the loginSecret and deviceSecret
+
         :param email: My.Jdownloader User email
         :param password: My.Jdownloader User password
-        :param domain: The domain , if is for Server (loginSecret) or Device (deviceSecret).  
+        :param domain: The domain , if is for Server (loginSecret) or Device (deviceSecret) 
         :return: secret hash
+
         """
         h = hashlib.sha256()
         h.update(email.lower().encode('utf-8')+password.encode('utf-8')+domain.lower().encode('utf-8'))
@@ -56,7 +79,7 @@ class myjdapi:
         Updates the serverEncryptionToken and deviceEncryptionToken
 
         """
-        if self.serverEncryptionToken=='':
+        if not self.serverEncryptionToken:
             oldtoken=self.loginSecret
         else:
             oldtoken=self.serverEncryptionToken            
@@ -68,9 +91,11 @@ class myjdapi:
         self.deviceEncryptionToken=h.digest()
     def __signaturecreate(self,key,data):
         """
-        Calculates the signature for the data given a key
+        Calculates the signature for the data given a key.
+
         :param key: 
         :param data:
+
         """
         h = hmac.new(key,data.encode('utf-8'),hashlib.sha256)
         signature=h.hexdigest()
@@ -78,8 +103,10 @@ class myjdapi:
     def __decrypt(self,secretServer,data):
         """
         Decrypts the data from the server using the provided token
+
         :param secretServer: 
         :param data:
+
         """
         iv=secretServer[:len(secretServer)//2]        
         key=secretServer[len(secretServer)//2:]
@@ -88,25 +115,29 @@ class myjdapi:
         return decrypted_data
     def __updateRid(self):
         """
-        Puts a new value to rid
+        Adds 1 to rid
         """
-        self.rid=int(time.time())
+        # self.rid=int(time.time())
+        self.rid=self.rid+1
     def connect(self,email,password):
-        """
-        Establish connection to api
+        """Establish connection to api
+
         :param email: My.Jdownloader User email
         :param password: My.Jdownloader User password
         :returns: boolean -- True if succesful, False if there was any error.
+
         """
         self.loginSecret=self.__secretcreate(email,password,"server")
         self.deviceSecret=self.__secretcreate(email,password,"device")
-        get="/my/connect?email="+urllib.parse.quote(email)+"&appkey="+urllib.parse.quote(self.appkey)+"&rid="+str(self.rid)
-        get+="&signature="+str(self.__signaturecreate(self.loginSecret,get))
-        url=self.api_url+get
-        response=requests.get(url)
-        if response.status_code != 200:
-            return False
-        text=self.__decrypt(self.loginSecret,response.text)
+        text=self.call("/my/connect","GET",rid=True,params=[("email",email),("appkey",self.appkey)])
+        print(text)
+        # get="/my/connect?email="+urllib.parse.quote(email)+"&appkey="+urllib.parse.quote(self.appkey)+"&rid="+str(self.rid)
+        # get+="&signature="+str(self.__signaturecreate(self.loginSecret,get))
+        # url=self.api_url+get
+        # response=requests.get(url)
+        # if response.status_code != 200:
+        #     return False
+        # text=self.__decrypt(self.loginSecret,response.text)
         jsondata=json.loads(text.decode('utf-8'))
         if jsondata['rid']!=self.rid:
             return False
@@ -117,8 +148,10 @@ class myjdapi:
         return True
     def reconnect(self):
         """
-        Restablish connection to api
+        Restablish connection to api.
+
         :returns: boolean -- True if succesful, False if there was any error.
+
         """
         get="/my/reconnect?sessiontoken="+self.sessiontoken+"&regaintoken="+self.regaintoken+"&rid="+str(self.rid)
         get+="&signature="+str(self.__signaturecreate(self.serverEncryptionToken,get))
@@ -140,7 +173,9 @@ class myjdapi:
     def disconnect(self):
         """
         Disconnects from  api
+
         :returns: boolean -- True if succesful, False if there was any error.
+
         """
         get="/my/disconnect?sessiontoken="+self.sessiontoken+"&rid="+str(self.rid)
         get+="&signature="+str(self.__signaturecreate(self.serverEncryptionToken,get))
@@ -160,7 +195,9 @@ class myjdapi:
     def getDevices(self):
         """
         Gets available devices. Use listDevices() to get the devices list. 
+
         :returns: boolean -- True if succesful, False if there was any error.
+
         """
         get="/my/listdevices?sessiontoken="+self.sessiontoken+"&rid="+str(self.rid)
         get+="&signature="+str(self.__signaturecreate(self.serverEncryptionToken,get))
@@ -179,13 +216,64 @@ class myjdapi:
     def listDevices(self):
         """
         Returns available devices. Use getDevices() to update the devices list. 
-        Each device in the list is a dictionary like this example: ::
-        {
+
+        Each device in the list is a dictionary like this example:
+        
+        { 
             'name': 'Device',
+
             'id': 'af9d03a21ddb917492dc1af8a6427f11',
+
             'type': 'jd'
+
         }
+
         :returns: list -- list of devices.
+
         """
         return self.__devices
+    def getDevice(self,deviceid):
+        """
+        Returns a jddevice instance of the device
+        
+        :param deviceid:
+        
+        """
+        
+        for device in self.__devices:
+            if device["id"]==deviceid:
+                return jddevice(device)
+        return False
 
+    def call(self,action,httpaction="GET",rid=True,params=False,postparams=False,device=False):
+        if (params):
+            call=action
+            for index,param in enumerate(params):
+                if index==0:
+                    call+="?"+param[0]+"="+urllib.parse.quote(param[1])
+                else:
+                    call+="&"+param[0]+"="+urllib.parse.quote(param[1])
+                    # Todo : Add an exception if the param is loginSecret so it doesn't get encoded.
+            if rid:
+                call+="&rid="+str(self.rid)
+            if not self.serverEncryptionToken:
+                call+="&signature="+str(self.__signaturecreate(self.loginSecret,call))
+            else:
+                call+="&signature="+str(self.__signaturecreate(self.serverEncryptionToken,get))
+        url=self.api_url+call
+        if (postparams):
+            pass
+        
+        if httpaction=="GET":
+            encryptedresp=requests.get(url)
+        elif httpaction=="POST":
+            pass
+        else:
+            pass
+        if encryptedresp.status_code != 200:
+            return 200
+        if not self.serverEncryptionToken:
+            response=self.__decrypt(self.loginSecret,encryptedresp.text) 
+        else:
+            response=self.__decrypt(self.serverEncryptionToken,encryptedresp.text)
+        return response
