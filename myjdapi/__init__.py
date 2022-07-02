e95eaaaf5fdcb6881a4c65682e0b2aaa19eae912
Marc Marquez Santamaria <mmsa1994@gmail.com>
2022-07-02 09:50:29 +0200

    Bump version

--- myjdapi/__init__.py
+++ myjdapi/__init__.py
@@ -34,4 +34,4 @@ from .exception import (
     MYJDUnknownException,
 )
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
--- setup.py
+++ setup.py
@@ -6,30 +6,31 @@ https://github.com/pypa/sampleproject
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 setup(
-    name='myjdapi',
-    version='1.1.4',
-    description='Library to use My.Jdownloader API in an easy way.',
+    name="myjdapi",
+    version="1.1.5",
+    description="Library to use My.Jdownloader API in an easy way.",
     long_description=long_description,
-    url='https://github.com/mmarquezs/My.Jdownloader-API-Python-Library/',
-    author='Marc Marquez Santamaria',
-    author_email='mmsa1994@gmail.com',
-    license='MIT',
+    url="https://github.com/mmarquezs/My.Jdownloader-API-Python-Library/",
+    author="Marc Marquez Santamaria",
+    author_email="mmsa1994@gmail.com",
+    license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
-    keywords='myjdapi jdownloader my.jdownloader api development',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['requests','pycryptodome'],
+    keywords="myjdapi jdownloader my.jdownloader api development",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    install_requires=["requests", "pycryptodome"],
 )
